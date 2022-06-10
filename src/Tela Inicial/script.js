539976b8880c03ba91cedde268ce6081b45e61d5
Igor Martins <iguinhoigor4@gmail.com>
2022-06-09 23:59:17 -0300

    feat: Colocar JSON nos profissionais bem avaliados #53

--- src/Tela Inicial/script.js
+++ src/Tela Inicial/script.js
@@ -27,24 +27,24 @@ const catalogoServicos = [
 
 const profissionaisBemAvaliados = [
   {
-    imagem: "./imgs/worker",
+    imagem: "./imgs/worker.png",
     nome: "Fred Silva",
     ocupacao: "Ator Profissional",
     descricao:
       "Nosso Chamaker prestou notórios serviços na arte da atuação nesse mês.",
   },
   {
-    imagem: "./imgs/worker",
+    imagem: "./imgs/worker.png",
     nome: "Nikolau 'Du Corte'",
     ocupacao: "Eletricista",
     descricao: "Nosso Chamaker foi destaque em reparos elétricos nesse mês.",
   },
   {
-    imagem: "./imgs/worker",
+    imagem: "./imgs/worker.png",
     nome: "Maicon Douglas",
     ocupacao: "Bombeiro",
     descricao:
-      "Nosso Chamamaker prestou notórios serviços como bombeiro nesse mês.",
+      "Nosso Chamamaker não dormiu no ponto e prestou notórios serviços como bombeiro nesse mês.",
   },
 ];
 
@@ -53,23 +53,50 @@ function imprimeDados() {
   // pega o container dos serviços a serem listados
   let containerServicos = document.getElementById("container-servicos");
 
-  // declara variável que irá receber os itens
+  // pega o container dos serviços a serem listados
+  let containerProfissionais = document.getElementById(
+    "container-bem-avaliados"
+  );
+
+  // declara variável que irá receber o catálogo
   let conteudoCatalogo = "";
+
+  // declara variável que irá receber os profissionais
+  let conteudoProfissionais = "";
 
   // executa item por item e salva dentro da variável
   for (let i = 0; i < catalogoServicos.length; i++) {
     conteudoCatalogo += `
-              <div class="servico-catalogo">
+            <div class="servico-catalogo">
                 <div class="container-img">
                     <img src="${catalogoServicos[i].imagem}" alt="Icone trabalho">
                 </div>
                 <h4>${catalogoServicos[i].titulo}</h4>
-             </div>
+            </div>
+    `;
+  }
+
+  // executa item por item e salva dentro da variável
+  for (let i = 0; i < 3; i++) {
+    conteudoProfissionais += `
+            <div class="bem-avaliados-item">
+                <div class="container-img">
+                    <img src="${profissionaisBemAvaliados[i].imagem}" alt="Icone trabalho">
+                </div>
+                <div class="container-texto">
+                    <h3>${profissionaisBemAvaliados[i].nome}</h3>
+                    <h4>${profissionaisBemAvaliados[i].ocupacao}</h4>
+                    <p>${profissionaisBemAvaliados[i].descricao}</p>
+                </div>
+            </div>
     `;
   }
 
   // coloca a variável no HTML da página
   containerServicos.innerHTML = conteudoCatalogo;
+
+  // coloca a variável no HTML da página
+  containerProfissionais.innerHTML = conteudoProfissionais;
 }
 
 // quando todos os itens da tela terminas de ser carregados, chama a função imprimeDados
