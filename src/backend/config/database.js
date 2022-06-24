3e2fba0fe296b12979b758f3168dd8ab2d5f2487
Nikolas Louret <nikoul.ret@gmail.com>
2022-06-24 00:11:48 -0300

    [Update] Resolução de conflitos de merge #65

--- src/backend/config/database.js
+++ src/backend/config/database.js
@@ -2,11 +2,7 @@ module.exports = {
     dialect: 'mysql',
     host: 'localhost',
     username: 'root',
-<<<<<<< HEAD
-    password: 'Macacox1@',
-=======
     password: 'admin',
->>>>>>> 2a594c959aaff450ef709b50406c9f8c6dea99e9
     database: 'dbchamanow',
     define: {
         timestamp: true,
